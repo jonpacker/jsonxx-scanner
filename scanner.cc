1a2b3ed426d2761f86550c411fb963e5343305cf
Jon Packer <contact@jonpacker.com>
2012-09-21 01:01:19 +0200

    Will now --theoretically-- read a buffer and spit out a number event. No way to compile, probably syntax errors, untested.

--- scanner.cc
+++ scanner.cc
@@ -8,12 +8,12 @@ namespace jsonxxbind {
 
   class scanner_fireall : public jsonxx::scanner_listener {
   public:
-    scanner_fireall(const v8::Handle<v8::Object> &jsObj) 
-      : jsonxx:scanner_listener() , jsObj(jsObj) {};
+    scanner_fireall(const v8::Handle<v8::Object> &js_obj) 
+      : jsonxx:scanner_listener() , js_obj(js_obj) {};
 
     void emit(int argc, v8::Handle<v8::Value> argv[]) {
-      v8::Handle<v8::Function> emit = jsObj->Get(v8::String::New("emit"));
-      emit->Call(jsObj, argc, argv);
+      v8::Handle<v8::Function> emit = js_obj->Get(v8::String::New("emit"));
+      emit->Call(js_obj, argc, argv);
     };
 
     void fire_event(const std::string& event_name, 
@@ -25,7 +25,7 @@ namespace jsonxxbind {
         v8::String::New(event_data.data())
       };
 
-      emit->Call(jsObj, 2, argv);
+      emit->Call(js_obj, 2, argv);
       scope->Close(v8::Undefined());
     };
 
@@ -34,7 +34,7 @@ namespace jsonxxbind {
       
       v8::Handle<Value> argv[] = { v8::String::New(event_name.data()) };
 
-      emit->Call(jsObj, 1, argv);
+      emit->Call(js_obj, 1, argv);
       scope->Close(v8::Undefined());
     };
 
@@ -43,12 +43,46 @@ namespace jsonxxbind {
     };
 
   private:
-    const Handle<Object> &jsObj;
+    const Handle<Object> &js_obj;
   };
   
   class scanner_iface : public node::ObjectWrap {
-    scanner_iface(jsonxx::scanner_listener& listener) 
-      : ObjectWrap(), scanner(new jsonxx::scanner(listener)) {};
+  public:
+    scanner_iface(v8::Handle<v8::Object> &js_parent) 
+      : ObjectWrap(), 
+        listener(new scanner_fireall(js_parent)),
+        scanner(new jsonxx::scanner(listener)) {};
+
+    ~scanner_iface() {
+      delete scanner;
+      delete listener;
+    };
+
+    static v8::Handle<v8::Value> New(const v8::Arguments &args) {
+      v8::Handle<v8::Object> holder = args.Holder();
+      scanner_iface *wrapper = new scanner_iface(holder);
+      wrapper->Wrap(holder);
+      return holder;
+    }
+
+    static v8::Handle<v8::Value> scan_bytes(const v8::Arguments &args) {
+      v8::Handle<v8::Object> buffer = args[0];
+      v8::Handle<v8::Object> self = args.Holder();
+      scanner_iface* scanner = node::ObjectWrap::Unwrap<scanner_iface>(self);
+
+      if (buffer->IsUndefined() || !node::Buffer::HasInstance(buffer)) {
+        return v8::ThrowException(v8::String::New("expected some dataz"));
+      }
+
+      char* buffer_contents = node::Buffer::Data(buffer);
+      int buffer_size = node::Buffer::Length(buffer);
+
+      scanner->scan(buffer_contents, buffer_contents + buffer_size);
+      return v8::Undefined();
+    }
+  private:
+    jsonxx::scanner *scanner;
+    jsonxx::scanner_listener *listener;
   };
 
 }
