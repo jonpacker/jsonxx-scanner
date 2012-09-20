9e818cb2bd315747da12e45083394b728e6437dd
Jon Packer <contact@jonpacker.com>
2012-09-20 21:43:32 +0200

    Added basic stream interface

--- scanner.js
+++ scanner.js
@@ -1,9 +1,28 @@
 var util = require('util'),
-    events = require('events');
+    events = require('events'),
+    jxx = require('../build/Release/jsonxx.node');
 
 function JxxScanner() {
   this.writable = true;
   this.buffer = null;
-}
+};
 
+util.inherits(JxxScanner, events.EventEmitter);
 
+JxxScanner.prototype.write = function(buffer, encoding) {
+  if (typeof buffer === 'string') {
+    this.jxx.scanStr(buffer, encoding);
+  } else {
+    this.jxx.scan(buffer);
+  }
+  return true;
+};
+
+JxxScanner.prototype.end = function(buffer, encoding) {
+  if (buffer) {
+    this.write(buffer, encoding);
+  }
+
+  this.emit('close');
+  this.writable = false;
+};
